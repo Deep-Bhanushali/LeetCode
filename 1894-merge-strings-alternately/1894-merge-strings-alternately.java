95af4b2f5b5090b7ee5a93ef69153364771c18f9
Mr-DAB <161147802+Deep-Bhanushali@users.noreply.github.com>
2025-05-14 12:43:06 +0530

    Time: 0 ms (100%), Space: 41.9 MB (45.23%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0,j=0,turn = 0;
+
+        StringBuilder res = new StringBuilder();
+
+        while(i < word1.length() && j < word2.length()){
+            res.append(word1.charAt(i++));
+            res.append(word2.charAt(j++));
+        }
+        if(i < word1.length()){
+            res.append(word1.substring(i));
+        }
+        
+        if(j < word2.length()){
+            res.append(word2.substring(j));
+        }
+
+        return res.toString();
+
+
+    }
+}
